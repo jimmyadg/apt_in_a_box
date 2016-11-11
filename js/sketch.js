448321d1699f4cc9b6c1564014a98e263b882ec8
jimmyadg <jimmyadg@gmail.com>
2016-11-10 21:05:25 -0500

    push

--- js/sketch.js
+++ js/sketch.js
@@ -19,7 +19,6 @@ function draw(){
   background(255,242,236);
   img.resize(imgW,imgH);
   image(img,windowWidth/2-imgW/2,windowHeight/2-imgH/2);
-  beginShape();
-  vertex();
+  
   console.log(aspectRatio);
 }
