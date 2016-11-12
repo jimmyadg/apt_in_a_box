84c7421e4a6309ef8d4d7078126ad50669f4cf90
jimmyadg <jimmyadg@gmail.com>
2016-11-11 23:54:43 -0500

    push

--- js/sketch.js
+++ js/sketch.js
@@ -1,24 +1,21 @@
 var img;
-
-
 function preload(){
   img = loadImage("./img/frame.png");
 }
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
-
   background(100);
-}
-
-
-function draw(){
   var aspectRatio = windowWidth/windowHeight;
   var imgW = 2796/aspectRatio /3;
   var imgH = 2242/aspectRatio /3;
   background(255,242,236);
   img.resize(imgW,imgH);
   image(img,windowWidth/2-imgW/2,windowHeight/2-imgH/2);
-  
   console.log(aspectRatio);
+}
+
+
+function draw(){
+  ellipse(mouseX,mouseY,10,10);
 }
